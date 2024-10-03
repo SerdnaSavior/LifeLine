3257a1f2d97bdc90849f6690e694c5670c60947f
SerdnaSavior <140349635+SerdnaSavior@users.noreply.github.com>
2024-10-03 17:33:38 -0400

    testing

--- src/main/java/com/example/demo/controller/Logincontroller.java
+++ src/main/java/com/example/demo/controller/Logincontroller.java
@@ -2,16 +2,16 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.service.annotation.GetExchange;
 
 @Controller
 
-public class Signupcontroller {
+public class Logincontroller {
     
-    @GetMapping("/signup")
+    @GetMapping("/login")
     public String signup() {
-        return "signup";
+        return "login";
     } 
 }
+
+
