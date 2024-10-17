c23c5d157363ce1bea1f20562304c33ae862038c
SerdnaSavior <140349635+SerdnaSavior@users.noreply.github.com>
2024-10-17 17:39:52 -0400

    testing

--- src/main/java/com/example/demo/controller/aboutuscontroller.java
+++ src/main/java/com/example/demo/controller/aboutuscontroller.java
@@ -4,14 +4,16 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.Users;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/aboutus")
 public class aboutuscontroller {
-@GetMapping("/aboutus")
+@GetMapping()
     public String aboutus(HttpSession session, Model model) {
         Users currentUser = (Users) session.getAttribute("currentUser");
 
