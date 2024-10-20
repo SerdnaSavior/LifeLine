6d85f1e848fe17d1eee046270cb7c962dae4ff8a
JoshuaBoswell02 <joshuaboswell1234@gmail.com>
2024-10-20 18:15:03 -0400

    Updated Create and View Invoice

---
+++ src/main/java/com/example/demo/controller/InvoicesController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.Users;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/viewInvoices")
+public class InvoicesController {
+@GetMapping()
+    public String Invoices(HttpSession session, Model model) {
+        Users currentUser = (Users) session.getAttribute("currentUser");
+
+            // If user is not logged in, redirect to the login page
+            if (currentUser != null) {
+                 // Add user info to the model to display on the dashboard
+            model.addAttribute("username", currentUser.getUsername());
+            model.addAttribute("user", currentUser);
+            }
+
+        return "viewInvoices";
+    } 
+}
